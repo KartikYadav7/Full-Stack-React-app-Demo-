a2203d4b94e420953fc39dbe807d78ee00c983c6
kartikyadav7 <raokartik7503@gmail.com>
2025-03-25 10:05:51 +0530

    final changes

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5000'],
+  origin: ['https://full-stack-react-app-demo-frontend.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
